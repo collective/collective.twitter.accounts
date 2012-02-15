9412902d09e6d7ec57b5f57c3b1fa5a6958d084f
Franco Pellegrini <frapell@gmail.com>
2012-02-15 17:26:25 -0300

    Added logging to the product

--- src/collective/twitter/accounts/browser/controlpanel.py
+++ src/collective/twitter/accounts/browser/controlpanel.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 from Products.Five import BrowserView
 from zope.component import getUtility
 
 from zope import schema
 from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm
 
 from plone.fieldsets.form import FieldsetsEditForm
 
@@ -25,10 +27,15 @@ from plone.registry.interfaces import IRegistry
 from collective.twitter.accounts.config import PLONE_CONSUMER_KEY
 from collective.twitter.accounts.config import PLONE_CONSUMER_SECRET
 
+from collective.twitter.accounts.config import PROJECTNAME
+
 from zope.component import getMultiAdapter
 
 from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+import logging
+
+logger = logging.getLogger(PROJECTNAME)
 
 consumer_vocabulary = SimpleVocabulary.fromValues([_(u"Plone default"), _(u"Custom")])
 
@@ -39,7 +46,7 @@ def make_terms(items):
 
 def TwitterApplications(context):
 
-    app_list = [("Plone default", _(u"Plone default")), 
+    app_list = [("Plone default", _(u"Plone default")),
                 ("Custom", _(u"Custom"))]
 
 
@@ -64,22 +71,22 @@ class ITwitterFieldSchema(Interface):
                                                   "application. Not used if "
                                                   "\"Plone default\" chosen."),
                                    required=False)
-                           
+
     consumer_secret = schema.TextLine(title=_(u'Consumer Secret'),
                                       description=_(u"Consumer secret for your "
                                                      "application. Not used if "
                                                      "\"Plone default\" chosen."),
                                       required=False)
-    
+
     oauth_token = schema.TextLine(required=False)
- 
-    
+
+
     oauth_token_secret = schema.TextLine(required=False)
-                      
+
     pincode = schema.TextLine(title=_(u'Token'),
                               description=_(u'Security token'),
                               required=True)
-    
+
 
 class TwitterControlPanelAdapter(SchemaAdapterBase):
 
@@ -87,20 +94,20 @@ class TwitterControlPanelAdapter(SchemaAdapterBase):
     implements(ITwitterFieldSchema)
 
     consumer = ""
-    consumer_key = ""   
-    consumer_secret = ""           
+    consumer_key = ""
+    consumer_secret = ""
     oauth_token = ""
     oauth_token_secret = ""
     pincode = ""
-    
-    
+
+
 class TwitterControlPanel(FieldsetsEditForm):
     """
     Twitter control panel view
     """
-    
+
     implements(ITwitterControlPanel)
-    
+
     template = ViewPageTemplateFile('./templates/twitter-control-panel.pt')
 
     label = _("Twitter setup")
@@ -108,76 +115,80 @@ class TwitterControlPanel(FieldsetsEditForm):
     form_name = _("Twitter setup")
     form_fields = form.FormFields(ITwitterFieldSchema)
     request_twitter_token = _(u"Request twitter token")
-            
-        
+
+
     def getAccounts(self):
         registry = getUtility(IRegistry)
         accounts = registry['collective.twitter.accounts']
-        
+
         return accounts
-        
+
     @form.action(_(u'label_add', default=u'Add'), name=u'add')
     def handle_add_action(self, action, data):
-        
+        logger.info("Adding account to the registry")
         consumer = data['consumer']
         consumer_key = data['consumer_key']
         consumer_secret = data['consumer_secret']
         oauth_token = data['oauth_token']
         oauth_token_secret = data['oauth_token_secret']
         pincode = data['pincode']
-        
+
         if consumer == "Plone default":
             consumer_key = PLONE_CONSUMER_KEY
             consumer_secret = PLONE_CONSUMER_SECRET
-            
+
         if (consumer_key and
             consumer_secret and
             oauth_token and
             oauth_token_secret and
             pincode):
-                
-            validate_token = getMultiAdapter((self.context, self.request), 
+
+            validate_token = getMultiAdapter((self.context, self.request),
                                              name='validate-token')
-        
-            access_token = validate_token(consumer, consumer_key, consumer_secret, 
+
+            access_token = validate_token(consumer, consumer_key, consumer_secret,
                                           oauth_token, oauth_token_secret, pincode)
-            
+
             if access_token:
-                
+
                 registry = getUtility(IRegistry)
                 accounts = registry['collective.twitter.accounts']
                 if not accounts:
                     accounts = {}
-                
+
                 username = access_token['screen_name']
                 oauth_token = access_token['oauth_token']
                 oauth_token_secret = access_token['oauth_token_secret']
-                
+
                 accounts[username] = \
-                                     {'consumer_key' : consumer_key, 
-                                      'consumer_secret' : consumer_secret, 
-                                      'oauth_token' : oauth_token, 
+                                     {'consumer_key' : consumer_key,
+                                      'consumer_secret' : consumer_secret,
+                                      'oauth_token' : oauth_token,
                                       'oauth_token_secret' : oauth_token_secret}
-                
+
                 registry['collective.twitter.accounts'] = accounts
-                
+
+                logger.info("Account added succesfully to the registry")
+
+                logger.info("Token: %s"%access_token)
                 self.status = _("Twitter account succesfully authorized.")
 
             else:
+                logger.info("Account was not added to the registry")
                 self.status = _("Could not authorize. Perhaps wrong token provided.")
         else:
             self.status = _("Missing data.")
 
 class RemoveAuthAccount(BrowserView):
-    
+
     def __call__(self, account_name):
         registry = getUtility(IRegistry)
         accounts = registry['collective.twitter.accounts']
         if not accounts:
             accounts = {}
-            
+
         try:
             del accounts[account_name]
         except:
             pass
-            
+
--- src/collective/twitter/accounts/browser/token.py
+++ src/collective/twitter/accounts/browser/token.py
@@ -1,5 +1,4 @@
 from Products.Five import BrowserView
-from zope.component import getUtility
 
 from collective.twitter.accounts.config import ACCESS_TOKEN_URL
 from collective.twitter.accounts.config import REQUEST_TOKEN_URL
@@ -8,70 +7,96 @@ from collective.twitter.accounts.config import AUTHORIZATION_URL
 from collective.twitter.accounts.config import PLONE_CONSUMER_KEY
 from collective.twitter.accounts.config import PLONE_CONSUMER_SECRET
 
+from collective.twitter.accounts.config import PROJECTNAME
+
 from urlparse import parse_qsl
 import oauth2 as oauth
+import logging
+
+logger = logging.getLogger(PROJECTNAME)
 
 class Validate(BrowserView):
     """
     View used to validate the provided token
     """
-    
-    def __call__(self, 
+
+    def __call__(self,
                  consumer,
-                 consumer_key, 
-                 consumer_secret, 
-                 oauth_token, 
+                 consumer_key,
+                 consumer_secret,
+                 oauth_token,
                  oauth_token_secret,
                  pincode):
-    
-    
+
+        logger.info("Validate Twitter token.")
+        logger.info("consumer=%s"%consumer)
+        logger.info("consumer_key=%s"%consumer_key)
+        logger.info("consumer_secret=%s"%consumer_secret)
+        logger.info("oauth_token=%s"%oauth_token)
+        logger.info("oauth_token_secret=%s"%oauth_token)
+        logger.info("pincode=%s"%pincode)
+
         if consumer == "Plone default":
             consumer_key = PLONE_CONSUMER_KEY
             consumer_secret = PLONE_CONSUMER_SECRET
- 
+
         signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
-        oauth_consumer = oauth.Consumer(key=consumer_key, 
+        oauth_consumer = oauth.Consumer(key=consumer_key,
                                         secret=consumer_secret)
-    
+
         token = oauth.Token(oauth_token, oauth_token_secret)
         token.set_verifier(pincode)
-    
+
         oauth_client  = oauth.Client(oauth_consumer, token)
-        resp, content = oauth_client.request(ACCESS_TOKEN_URL, 
-                                             method='POST', 
+
+        logger.info("OAuth client created.")
+
+        resp, content = oauth_client.request(ACCESS_TOKEN_URL,
+                                             method='POST',
                                              body='oauth_verifier=%s' % pincode)
+
+        logger.info("Response: %s" % resp)
         access_token  = dict(parse_qsl(content))
-    
+
         if resp['status'] != '200':
             #ERROR
             return False
         else:
             #Success
+            logger.info("Success! token: %s" % access_token)
             return access_token
 
 
 class Request(BrowserView):
     """
-    View used to generate a URL used to request a token to access a 
+    View used to generate a URL used to request a token to access a
     twitter account
     """
-    
+
     def __call__(self, consumer, consumer_key=None, consumer_secret=None):
+        logger.info("Request Twitter token.")
+        logger.info("consumer=%s"%consumer)
+        logger.info("consumer_key=%s"%consumer_key)
+        logger.info("consumer_secret=%s"%consumer_secret)
 
         if consumer == "Plone default":
             consumer_key = PLONE_CONSUMER_KEY
             consumer_secret = PLONE_CONSUMER_SECRET
-    
+
         if not consumer_key or not consumer_secret:
             return False
-    
+
         signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
-        oauth_consumer = oauth.Consumer(key=consumer_key, 
+        oauth_consumer = oauth.Consumer(key=consumer_key,
                                         secret=consumer_secret)
- 
+
         oauth_client = oauth.Client(oauth_consumer)
 
+        logger.info("Oauth client created, contacting Twitter")
+
         resp, content = oauth_client.request(REQUEST_TOKEN_URL, 'GET')
+
+        logger.info("Response: %s" % resp)
 
         if resp['status'] != '200':
             #ERROR
@@ -82,7 +107,10 @@ class Request(BrowserView):
             # We return the URL needed to allow access to twitter, and we also
             # include the oauth_token and oauth_token_secret to be splitted
             # in Javascript. This is ugly.
-            return '%s?oauth_token=%s&%s&%s' % (AUTHORIZATION_URL, 
+            logger.info("URL to use: %s?oauth_token=%s"%(AUTHORIZATION_URL,
+                                                request_token['oauth_token']))
+
+            return '%s?oauth_token=%s&%s&%s' % (AUTHORIZATION_URL,
                                                 request_token['oauth_token'],
                                                 request_token['oauth_token'],
                                                 request_token['oauth_token_secret'])
