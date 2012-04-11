233fdc197752e90959bad9cb0d153f1c9752b43c
hvelarde <hector.velarde@gmail.com>
2012-04-10 21:10:02 -0500

    updated package classifiers

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 import os
 
 version = '1.0.1'
+long_description = open("README.rst").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='collective.twitter.accounts',
       version=version,
       description="Provide Twitter integration for a Plone Site",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
+      long_description=long_description,
       classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
       author='Franco Pellegrini',
@@ -25,17 +37,14 @@ setup(name='collective.twitter.accounts',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          'python-twitter==0.8.2',
-          'plone.app.registry',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'python-twitter',
+        'plone.app.registry',
+        ],
       extras_require={
         'test': ['plone.app.testing'],
         },
       entry_points="""
-      # -*- Entry points: -*-
-
       [z3c.autoinclude.plugin]
       target = plone
       """,
